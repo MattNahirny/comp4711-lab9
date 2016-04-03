5175e49605436907a4f227c304d6f6f1a6525347
MattNahirny <mnahirny1@my.bcit.ca>
2016-04-03 14:41:54 -0700

    auth controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -18,6 +18,18 @@ class Auth extends Application
 
     public function submit()
     {
+        $key = $_POST['userid'];
+        $user = $this->users->get($key);
+        if (password_verify($this->input->post('password'),$user->password)) {
+            $this->session->set_userdata('userID',$key);
+            $this->session->set_userdata('userName',$user->name);
+            $this->session->set_userdata('userRole',$user->role);
+        }
+        redirect('/');
+    }
 
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
     }
 }
